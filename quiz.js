c3c7a0c3503f8d535e973205bfef39bc49b4951d
redcreeper14385 <mounderfod@gmail.com>
2021-02-28 14:57:12 +0000

    Fix quiz code for Organisation.

--- quiz.js
+++ quiz.js
@@ -185,6 +185,8 @@ var submitButton = document.getElementById('submit');
 switch(window.location.pathname) {
     case "/chem/salts_acids.html":
         generateQuiz(salts_acids, quizContainer, resultsContainer, submitButton);
+    case "/bio/organisation.html":
+        generateQuiz(organisation, quizContainer, resultsContainer, submitButton);
     default:
         quizContainer.innerHTML = "<p>A quiz has not been made for this topic yet, sit tight!<p>"
 }
