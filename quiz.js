dfdf329a73a574249a1f5f1ddef708420f0786a2
redcreeper14385 <mounderfod@gmail.com>
2021-02-28 14:55:31 +0000

    Organisation notes

--- quiz.js
+++ quiz.js
@@ -125,6 +125,59 @@ var salts_acids = [
     }
 ]
 
+var organisation = [
+    {
+        question: "Which of the following is NOT a level of organisation?",
+        answers: {
+            a: "DNA",
+            b: "The heart",
+            c: "Tissues",
+            d: "Organ systems"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which of these is the correct order of organisation, from largest to smallest?",
+        answers: {
+            a: "Cells, DNA, Organs, Organ systems, Tissues, Organisms",
+            b: "DNA, Cells, Organs, Tissues, Organ systems, Organisms",
+            c: "DNA, Cells, Tissues, Organs, Organ systems, Organisms",
+            d: "DNA, Tissues, Cells, Organs, Organ systems, Organisms"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which of these is an example of an organ?",
+        answers: {
+            a: "The liver",
+            b: "Muscles",
+            c: "The gut",
+            d: "The endocrine system"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which of these statements is false?",
+        answers: {
+            a: "The heart is an organ.",
+            b: "Tissues are groups of cells.",
+            c: "Not all cells contain DNA.",
+            d: "Organs contain tissues with a similar purpose."
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Give the correct definition for a cell.",
+        answers: {
+            a: "A group of tissues working together.",
+            b: "The building blocks of living organisms, which perform important functions.",
+            c: "A living thing containing several organ systems.",
+            d: "An organ system which contains the heart."
+        },
+        correctAnswer: "b"
+    }
+]
+
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
@@ -132,4 +185,6 @@ var submitButton = document.getElementById('submit');
 switch(window.location.pathname) {
     case "/chem/salts_acids.html":
         generateQuiz(salts_acids, quizContainer, resultsContainer, submitButton);
+    default:
+        quizContainer.innerHTML = "<p>A quiz has not been made for this topic yet, sit tight!<p>"
 }
